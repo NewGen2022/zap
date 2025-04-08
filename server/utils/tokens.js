a171e63adba8c546e742e72079a674ced0999ac7
NewGen2022 <amirgimari@gmail.com>
2025-04-08 16:41:28 +0300

    added creation of the tokens

---
+++ server/utils/tokens.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const createAccessToken = async (user) => {
+    // Create the access token (short-lived)
+    return jwt.sign(
+        {
+            userId: user.id,
+            username: user.username,
+        },
+        process.env.JWT_ACCESS_SECRET,
+        { expiresIn: '1h', algorithm: 'HS256' }
+    );
+};
+
+const createRefreshToken = async (user) => {
+    return jwt.sign(
+        { userId: user.id, username: user.username },
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: '3d', algorithm: 'HS256' }
+    );
+};
+
+module.exports = { createAccessToken, createRefreshToken };
