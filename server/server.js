007b09b4747d3755abd26a43eef744f3ad782872
NewGen2022 <amirgimari@gmail.com>
2025-04-08 16:40:50 +0300

    added cookies middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRouter = require('./routes/auth');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,7 @@ app = express();
 
 app.use(express.json()); // for parsing JSON bodies
 app.use(express.urlencoded({ extended: true })); // for parsing form data
+app.use(cookieParser()); // for parsing cookies
 
 app.use('/api', authRouter);
 
