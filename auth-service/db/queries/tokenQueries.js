619befa560c0eff0b3486047765c10e00f7a300a
NewGen2022 <amirgimari@gmail.com>
2025-07-10 22:00:32 +0300

    added queries for: getting by verification token; and updating token used status

--- auth-service/db/queries/tokenQueries.js
+++ auth-service/db/queries/tokenQueries.js
@@ -16,8 +16,55 @@ const addToken = async (userId, hash) => {
 
         return token;
     } catch (err) {
-        throw new Error('Error while adding new token:', err);
+        throw new Error(`Error while adding new token: ${err}`);
     }
 };
 
-module.exports = { addToken };
+const getByVerificationToken = async (tokenHash) => {
+    if (!tokenHash) throw new Error('No token is provided to get by');
+
+    try {
+        const userId = await prismaClient.verificationToken.findUnique({
+            where: {
+                tokenHash,
+            },
+            select: {
+                id: true,
+                userId: true,
+                expiresAt: true,
+                isUsed: true,
+            },
+        });
+
+        return userId;
+    } catch (err) {
+        throw new Error(
+            `Error while getting by verification token: ${err.message}`
+        );
+    }
+};
+
+// set it as USED
+const updateVerificationToken = async (tokenId) => {
+    if (!tokenId) throw new Error('No token id is provided');
+
+    try {
+        await prismaClient.verificationToken.update({
+            where: {
+                id: tokenId,
+            },
+            data: { isUsed: true },
+        });
+    } catch (err) {
+        console.error(err);
+        throw new Error(
+            `Error while updating verification token: ${err.message}`
+        );
+    }
+};
+
+module.exports = {
+    addToken,
+    getByVerificationToken,
+    updateVerificationToken,
+};
