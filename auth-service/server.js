22dcdb30724afb5e1fbda6fbbcc040172fc2cd69
NewGen2022 <amirgimari@gmail.com>
2025-07-23 17:32:40 +0300

    added middleware for getting logging id; added logging

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const requestId = require('./middleware/requestId');
+const logger = require('./utils/logger');
+
 const userRouter = require('./routes/user');
 const passwordRouter = require('./routes/password');
 const tokenRouter = require('./routes/token');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
 
 const PORT = process.env.AUTH_SERVICE_PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV || 'dev';
 
 const server = express();
+logger.info('Auth service booting...', {
+    action: 'boot',
+    env: NODE_ENV,
+    port: PORT,
+});
 
 // Parse incoming JSON bodies (application/json)
 server.use(express.json());
@@ -18,6 +27,9 @@ server.use(express.urlencoded({ extended: true }));
 // Parse httpOnly cookies, needed for JWT refresh & access token cookies
 server.use(cookieParser());
 
+// Adds id for every request
+server.use(requestId);
+
 // Route for all auth-related operations (register, login, reset password, etc.)
 server.use('/api/v1/auth', userRouter);
 server.use('/api/v1/auth', passwordRouter);
@@ -25,5 +37,5 @@ server.use('/api/v1/auth', tokenRouter);
 
 // Start listening for HTTP requests
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info('Auth service listening', { action: 'listen', port: PORT });
 });
